ec1143467ae357ad5716b3750c08d97156f29fcf
Rocky108 <dougald2001@gmail.com>
2019-10-08 08:39:28 -0500

    Initial commit

---
+++ Week2.py
@@ -0,0 +1,48 @@
+x = "There are %d types of people." %10\
+bianary = "bianary"
+doNot = "don't"
+y = "Those who know %s and those who know %s." % (bianary, doNot)
+
+print(x)
+print(y)
+
+print("I said: %r" % x)
+print("I also said: '%s' ." % y)
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+print(joke_evaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+print("mary had a little lamb.")
+print("Its fleece was white as %s." % 'snow')
+print("And everywhere that mary went.")
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1+end2+end3+end3+end4+end5+end6+end7+end8+end9+end10+end11+end12)
+
+# More formatting
+formatter = "%r %r %r %r"
+print(formatter % (1,2,3,4))
+print(formatter % ("one","two","three","four"))
+print(formatter % (True,False,False,True))
+print(formatter % (formatter,formatter,formatter,formatter))
+
+# Why did i use %r instead of %s?
