d42eca2daa1ff32276cab40a4c020af0fbdab07d
Rocky108 <dougald2001@gmail.com>
2019-10-08 08:46:00 -0500

    added the octothorps for the project day i missed

--- Week2.py
+++ Week2.py
@@ -1,6 +1,10 @@
+#This string is talking about what kinds of people there are
 x = "There are %d types of people." %10\
+# This is a string for the first type of person
 bianary = "bianary"
+# this is the string for the second type of person
 doNot = "don't"
+# This string adds the two types together
 y = "Those who know %s and those who know %s." % (bianary, doNot)
 
 print(x)
@@ -9,15 +13,19 @@ print(y)
 print("I said: %r" % x)
 print("I also said: '%s' ." % y)
 
+# Hilarious = false due to the joke not being funny
 hilarious = False
+
 joke_evaluation = "Isn't that joke so funny?! %r"
 print(joke_evaluation % hilarious)
 
+# These two are the two types of strings
 w = "This is the left side of..."
 e = "a string with a right side."
 
 print(w + e)
 
+# start of the 10/8 work day
 print("mary had a little lamb.")
 print("Its fleece was white as %s." % 'snow')
 print("And everywhere that mary went.")
